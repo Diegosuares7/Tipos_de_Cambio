b73db1d42877c3e104d31d623e47ba036ac4adb6
Juan Avila <juan.avila@dirmod.com>
2024-02-09 15:19:22 +0000

    Merged PR 10790: #50941: arquitectura inicial y configuracion de eslint
    #50941: arquitectura inicial y configuracion de eslint
    
    Related work items: #50941

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/stylistic',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js', 'dist/', 'node_modules/'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-confusing-void-expression': 'error',
+    '@typescript-eslint/no-mixed-enums': 'error',
+    '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+    '@typescript-eslint/no-unnecessary-condition': 'error',
+    '@typescript-eslint/no-unnecessary-qualifier': 'error',
+    '@typescript-eslint/prefer-function-type': 'off',
+    '@typescript-eslint/prefer-literal-enum-member': 'error',
+    'linebreak-style': ['error', 'unix'],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'variable',
+        format: ['UPPER_CASE', 'camelCase', 'PascalCase'],
+      },
+
+      {
+        selector: ['function', 'classMethod', 'classProperty', 'parameter'],
+        format: ['camelCase'],
+      },
+      {
+        selector: ['class', 'interface'],
+        format: ['PascalCase'],
+      },
+    ],
+  },
+};
